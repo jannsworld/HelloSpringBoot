42279b4944f415675709cc9ab9f82eece7b261d7
jann <jjung5459@gmail.com>
2022-06-25 23:59:47 +0900

    Initial commit

---
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -0,0 +1,11 @@
+package com.example.hello.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // REST API 처리하는 Controller
+@RequestMapping("/api")
+public class ApiController {
+
+}
