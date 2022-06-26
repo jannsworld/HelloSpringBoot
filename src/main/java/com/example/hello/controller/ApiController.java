a984a31095b8fd4df53be0dd1fb764a0dd8b513d
jann <jjung5459@gmail.com>
2022-06-26 23:55:38 +0900

    API Controller

--- src/main/java/com/example/hello/controller/ApiController.java
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.example.hello.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello SpringBoot!";
+
+    }
 }
