f3653a9b507860759598c0b9acb10e0c54439964
jann <jjung5459@gmail.com>
2022-07-04 23:43:15 +0900

    POST Mapping

---
+++ src/main/java/com/example/hello/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requestData) {
+
+        requestData.forEach((key, value) -> {
+            System.out.println("key :" + key);
+            System.out.println("value :" + key);
+        });
+
+
+    }
+}
---
+++ src/main/java/com/example/hello/dto/PostRequestDto.java
@@ -0,0 +1,63 @@
+package com.example.hello.dto;
+
+
+public class PostRequestDto {
+
+    private String account;
+    private String email;
+    private String address;
+    private String password;
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String accaccountout) {
+        this.account = account;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PostApiController{" +
+                "account='" + account + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
+
