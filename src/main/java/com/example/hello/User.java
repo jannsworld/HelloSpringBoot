cde33e3d4b22deb435957a930a3b5df5200e2c37
jann <jjung5459@gmail.com>
2022-07-08 23:47:55 +0900

    ObjectMapper Test

---
+++ src/main/java/com/example/hello/User.java
@@ -0,0 +1,32 @@
+package com.example.hello;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User() {
+        this.name = null;
+        this.age = 0;
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.hello;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class HelloApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("===");
     }
+
+    // Text JSON -> Object
+    // Object -> Text JSON
+
+    // controller rea json(text) -> object
+    // response object -> json(text)
+
+    var objectMapper = new ObjectMapper();
+
+    //object -> text
+    var user = new User("jann", 30);
+
+    var text : String = objectMapper.writeValueAsString(user);
+
+    // text -> object
+    var objectUser : User = objectMapper.readValue(text, User.class);
+
+
+
 
 }
